dc300caf4bcc544311fe263f17fc0018df82833b
arixlin <linmingzxx@gmail.com>
2018-06-21 18:10:29 +0800

    restore model train

--- run_cnn.py
+++ run_cnn.py
@@ -138,6 +138,75 @@ def train():
             break
 
 
+def restore_train():
+    """
+    restore the model and then continue train
+    :return:
+    """
+    # 载入训练集与验证集
+    start_time = time.time()
+    x_train, y_train = process_file(train_dir, word_to_id, cat_to_id, config.seq_length)
+    x_val, y_val = process_file(val_dir, word_to_id, cat_to_id, config.seq_length)
+    time_dif = get_time_dif(start_time)
+    print("Time usage:", time_dif)
+
+    print("Restore model")
+    session = tf.Session()
+    session.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.restore(sess=session, save_path=save_path)
+
+    print('Training and evaluating...')
+    start_time = time.time()
+    total_batch = 0  # 总批次
+    best_acc_val = 0.0  # 最佳验证集准确率
+    last_improved = 0  # 记录上一次提升批次
+    require_improvement = 1000  # 如果超过1000轮未提升，提前结束训练
+
+    flag = False
+    for epoch in range(config.num_epochs):
+        print('Epoch:', epoch + 1)
+        batch_train = batch_iter(x_train, y_train, config.batch_size)
+        for x_batch, y_batch in batch_train:
+            feed_dict = feed_data(x_batch, y_batch, config.dropout_keep_prob)
+
+            # if total_batch % config.save_per_batch == 0:
+            #     # 每多少轮次将训练结果写入tensorboard scalar
+            #     s = session.run(merged_summary, feed_dict=feed_dict)
+            #     writer.add_summary(s, total_batch)
+
+            if total_batch % config.print_per_batch == 0:
+                # 每多少轮次输出在训练集和验证集上的性能
+                feed_dict[model.keep_prob] = 1.0
+                loss_train, acc_train = session.run([model.loss, model.acc], feed_dict=feed_dict)
+                loss_val, acc_val = evaluate(session, x_val, y_val)  # todo
+
+                if acc_val > best_acc_val:
+                    # 保存最好结果
+                    best_acc_val = acc_val
+                    last_improved = total_batch
+                    saver.save(sess=session, save_path=save_path)
+                    improved_str = '*'
+                else:
+                    improved_str = ''
+
+                time_dif = get_time_dif(start_time)
+                msg = 'Iter: {0:>6}, Train Loss: {1:>6.2}, Train Acc: {2:>7.2%},' \
+                      + ' Val Loss: {3:>6.2}, Val Acc: {4:>7.2%}, Time: {5} {6}'
+                print(msg.format(total_batch, loss_train, acc_train, loss_val, acc_val, time_dif, improved_str))
+
+            session.run(model.optim, feed_dict=feed_dict)  # 运行优化
+            total_batch += 1
+
+            if total_batch - last_improved > require_improvement:
+                # 验证集正确率长期不提升，提前结束训练
+                print("No optimization for a long time, auto-stopping...")
+                flag = True
+                break  # 跳出循环
+        if flag:  # 同上
+            break
+
+
 def test():
     print("Loading test data...")
     start_time = time.time()
@@ -182,8 +251,8 @@ def test():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2 or sys.argv[1] not in ['train', 'test']:
-        raise ValueError("""usage: python run_cnn.py [train / test]""")
+    if len(sys.argv) != 2 or sys.argv[1] not in ['train', 'test', 'restore']:
+        raise ValueError("""usage: python run_cnn.py [train / test/ restore]""")
 
     print('Configuring CNN model...')
     config = TCNNConfig()
@@ -196,5 +265,7 @@ if __name__ == '__main__':
 
     if sys.argv[1] == 'train':
         train()
+    elif sys.argv[1] == 'restore':
+        restore_train()
     else:
         test()
